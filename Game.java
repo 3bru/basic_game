021f92afcf7605c5e189e5a86d18ed0470fc4a2a
Ebru Arslan <16ebruarslan@gmail.com>
2019-09-23 15:52:56 +0300

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Game {
+
+  int value;
+  String player;
+
+  public Game(){
+
+  }
+
+  public Game (int value,String player){
+    this.value = value;
+    this.player = player;
+  }
+
+  public int getValue(){
+    return value;
+  }
+
+  public void setValue(int value){
+    this.value = value;
+  }
+
+
+  public void setPlayer(String player2) {
+    this.value = value;
+  }
+}
